9356f8aaa713bf75dfa0ec63b3e3950f37a23de7
bispoalef <alef.b.dos.santos.1993@gmail.com>
2023-04-29 17:45:35 -0300

    rename

--- src/main/java/com/bispoalef/soap_api/SoapApplication.java
+++ src/main/java/com/bispoalef/soap_api/SoapApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SoapApiApplication {
+public class SoapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SoapApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SoapApplication.class, args);
+    }
 
 }
