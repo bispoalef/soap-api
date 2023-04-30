3dee61f1950d6199f80b90b46159fa60fb5cd8e9
bispoalef <alef.b.dos.santos.1993@gmail.com>
2023-04-29 21:13:22 -0300

    teste unitario

--- src/test/java/com/bispoalef/soap_api/SoapApplicationTests.java
+++ src/test/java/com/bispoalef/soap_api/SoapApplicationTests.java
@@ -1,13 +1,31 @@
 package com.bispoalef.soap_api;
 
+import com.bispoalef.soap_api.controller.ProdutoController;
+import com.bispoalef.soap_api.entites.Departamento;
+import com.bispoalef.soap_api.entites.Produtos;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SoapApplicationTests {
 
+	@InjectMocks
+	ProdutoController produtoController;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("fazer requesição e retornar ok")
+	void fazerRequisicaoAposRetornarDados() {
+
+		List<Produtos> retorno = produtoController.buscarProdutos();
+
+		Assertions.assertNotNull(retorno);
+        Assertions.assertEquals(3, retorno.size());
 	}
 
 }
