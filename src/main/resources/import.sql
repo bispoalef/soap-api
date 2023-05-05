3b19ff712a1c57c73574575de85b4beb68671f4a
bispoalef <alef.b.dos.santos.1993@gmail.com>
2023-05-04 22:33:59 -0300

    adicionando dependencias e adicioando informações nas tabelas do banco de dados

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_departamento (nome) VALUES ('Tech');
+INSERT INTO tb_departamento (nome) VALUES ('Pet');
+
+INSERT INTO tb_produto (nome, preco, departamento_id) VALUES ('Macbook Pro', 4000.0, 1);
+INSERT INTO tb_produto (nome, preco, departamento_id) VALUES ('Pc Gamer', 5000.0, 1);
+INSERT INTO tb_produto (nome, preco, departamento_id) VALUES ('Pet House', 300.0, 2);
